fc759acbaec6d0551537ead9c1b13bf6ba8beb13
ZoroDerVonCodier <daveb@signalpop.com>
2020-12-18 13:19:08 -0800

    Added support for expected model inputs and outputs.

--- csharp/src/Microsoft.ML.OnnxTraining/TrainingSession.cs
+++ csharp/src/Microsoft.ML.OnnxTraining/TrainingSession.cs
@@ -3,6 +3,7 @@
 // Licensed under the MIT License.
 
 using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,6 +13,8 @@ namespace Microsoft.ML.OnnxTraining
     public class TrainingSession : IDisposable
     {
         TrainingParameters m_param = new TrainingParameters();
+        private OrtValueCollection m_expectedInputs = new OrtValueCollection(IntPtr.Zero);
+        private OrtValueCollection m_expectedOutputs = new OrtValueCollection(IntPtr.Zero);
         bool _disposed = false;
 
         public TrainingSession()
@@ -52,6 +55,8 @@ namespace Microsoft.ML.OnnxTraining
             if (disposing)
             {
                 m_param.Dispose();
+                m_expectedInputs.Dispose();
+                m_expectedOutputs.Dispose();
             }
 
             _disposed = true;
@@ -67,17 +72,45 @@ namespace Microsoft.ML.OnnxTraining
 
         public void Initialize(OrtEnv env)
         {
-            NativeApiStatus.VerifySuccess(NativeMethodsTraining.OrtInitializeTraining(env.DangerousGetHandle(), m_param.Handle));
+            NativeApiStatus.VerifySuccess(NativeMethodsTraining.OrtInitializeTraining(env.DangerousGetHandle(), m_param.DangerousGetHandle(), m_expectedInputs.DangerousGetHandle(), m_expectedOutputs.DangerousGetHandle()));
+
+            m_param.ExpectedInputs = getTensorDefs(m_expectedInputs);
+            m_param.ExpectedOutputs = getTensorDefs(m_expectedOutputs);
+        }
+
+        private List<KeyValuePair<string, List<int>>> getTensorDefs(OrtValueCollection col)
+        {
+            List<KeyValuePair<string, List<int>>> rgDefs = new List<KeyValuePair<string, List<int>>>();
+            int nCount = col.Count;
+
+            for (int i = 0; i < nCount; i++)
+            {
+                string strName;
+                OrtValue val = col.GetAt(i, out strName);
+                DisposableNamedOnnxValue shape = DisposableNamedOnnxValue.CreateTensorFromOnnxValue(strName, val);
+                Tensor<long> shapet = shape.AsTensor<Int64>();
+                List<int> rgShape = new List<int>();
+
+                for (int j = 0; j < shapet.dimensions[0]; j++)
+                {
+                    long nDim = shapet.GetValue(j);
+                    rgShape.Add((int)nDim);
+                }
+
+                rgDefs.Add(new KeyValuePair<string, List<int>>(strName, rgShape));
+            }
+
+            return rgDefs;
         }
 
         public void RunTraining()
         {           
-            NativeApiStatus.VerifySuccess(NativeMethodsTraining.OrtRunTraining(m_param.Handle));
+            NativeApiStatus.VerifySuccess(NativeMethodsTraining.OrtRunTraining(m_param.DangerousGetHandle()));
         }
 
         public void EndTraining()
         {
-            NativeApiStatus.VerifySuccess(NativeMethodsTraining.OrtEndTraining(m_param.Handle));
+            NativeApiStatus.VerifySuccess(NativeMethodsTraining.OrtEndTraining(m_param.DangerousGetHandle()));
         }
     }
 }
